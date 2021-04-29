8eb235af2b5ebb08cef396c583ce6bcc38bead0e
Samuel <59765994+samuelng87@users.noreply.github.com>
2021-04-29 16:25:07 +1000

    first push

---
+++ Assesment3.java
@@ -0,0 +1,45 @@
+package assesment3;
+
+import java.util.Scanner;
+
+import java.text.SimpleDateFormat;  
+
+import java.util.Date;  
+
+public class Assesment3 {
+
+	
+	public static void main(String[] args) {
+		
+
+Scanner sc=new Scanner(System.in);
+Date date=java.util.Calendar.getInstance().getTime();  
+
+
+
+for (int i=0;i<70;i++) {
+	System.out.print("*");
+}
+
+System.out.println("\n\n\n WELCOME TO RIDE-SHARING CHARGES ESTIMATOR AND COMPARISON SYSTEM\n\n");
+		
+		
+for (int i=0;i<70;i++) {
+	System.out.print("*");
+}
+		
+
+System.out.print("\n\n\nDeveloped by Koki, Ashmit, Saroj and Samuel Student IDs add your id here K210032 \n"
+		+ "OODP101 Object Oriented Design and Programming\n"
+		+ date
+		+ "\n\n\n");
+
+
+for (int i=0;i<70;i++) {
+	System.out.print("*");
+}
+		
+		
+	}
+
+}
