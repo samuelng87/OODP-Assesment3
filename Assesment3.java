cfabab40d43f894be9d998a6423edcf455283de7
Samuel <59765994+samuelng87@users.noreply.github.com>
2021-05-05 16:15:58 +1000

    second push

--- Assesment3.java
+++ Assesment3.java
@@ -11,35 +11,44 @@ public class Assesment3 {
 	
 	public static void main(String[] args) {
 		
-
+		
 Scanner sc=new Scanner(System.in);
 Date date=java.util.Calendar.getInstance().getTime();  
 
 
+printStars();
 
-for (int i=0;i<70;i++) {
-	System.out.print("*");
-}
 
-System.out.println("\n\n\n WELCOME TO RIDE-SHARING CHARGES ESTIMATOR AND COMPARISON SYSTEM\n\n");
+System.out.println("\n\n WELCOME TO RIDE-SHARING CHARGES ESTIMATOR AND COMPARISON SYSTEM\n\n");
 		
-		
-for (int i=0;i<70;i++) {
-	System.out.print("*");
-}
+printStars();
+
 		
 
-System.out.print("\n\n\nDeveloped by Koki, Ashmit, Saroj and Samuel Student IDs add your id here K210032 \n"
+System.out.print("\n\nDeveloped by Koki, Ashmit, Saroj and Samuel Student IDs add your id here K210032 \n"
 		+ "OODP101 Object Oriented Design and Programming\n"
 		+ date
-		+ "\n\n\n");
+		+ "\n\n");
 
 
-for (int i=0;i<70;i++) {
-	System.out.print("*");
-}
-		
+printStars();
+
+
+
+
 		
 	}
+	
+	
+	
+	public static void printStars() {
+		for(int i=0;i<100;i++) {
+			System.out.print("*");
+		}
+		System.out.println();	
 
 }
+}
+
+
+
