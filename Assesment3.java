0f1f4f4ca55e952449d68e24f524f429a8351742
kguragain1 <kguragain1@gmail.com>
2021-05-13 02:22:29 +1000

    Update Assesment3.java

--- Assesment3.java
+++ Assesment3.java
@@ -1,42 +1,42 @@
 package assesment3;
 import java.util.Scanner;
-import java.text.SimpleDateFormat;  
-import java.util.Date;  
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Assesment3 {
 
-	
+
 	public static void main(String[] args) {
-	
+
 		nameEntry();
 		printStars();
-		welcomeMessage();		
+		welcomeMessage();
 		printStars();
 		int option = printMenu();
 		handleMenu(option);
-                
-		
+
+
 	}
-	
+
 
 	public static void nameEntry() {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter your name");
 		String name=sc.next();
-		
-		
-	}	
-    
+
+
+	}
+
 	
-	
+
 public static void welcomeMessage() {
 	Scanner sc=new Scanner(System.in);
-	Date date=java.util.Calendar.getInstance().getTime(); 
+	Date date=java.util.Calendar.getInstance().getTime();
 	System.out.println("\n\nWELCOME TO RIDE-SHARING CHARGES ESTIMATOR AND COMPARISON SYSTEM");
 	System.out.println("Developed by Koki, Ashmit, Saroj and Samuel Student IDs add your id here K210032");
 	System.out.println("OODP101 Object Oriented Design and Programming");
-	System.out.println(date + "\n");		
-			
+	System.out.println(date + "\n");
+
 
 }
 
@@ -48,7 +48,7 @@ public static void printStars() {
 	for(int i=0;i<100;i++) {
 	 System.out.print("*");
 			}
-		
+
 }
 
 public static int kiloMeters(){
@@ -57,60 +57,60 @@ public static int kiloMeters(){
     int kms = sc.nextInt();
     return kms;
 }
-	
 
-public static int printMenu() { 
-	
-	
+
+public static int printMenu() {
+
+
 	int userSelection;
 	Scanner sc= new Scanner(System.in);
-	
-	
+
+
 	System.out.println("\n\n1. Display charges under UBER");
 	System.out.println("2. Display charges under DIDI");
 	System.out.println("3. Display Charges under OLA");
 	System.out.println("4. Show Report");
 	System.out.println("5. Exit");
-	printStars(); 
+	printStars();
 	userSelection=sc.nextInt();
-	
-	
+
+
 		while(userSelection<1 || userSelection>5) {
 			System.out.println("Error! ::::::::: Please Enter number 1 to 5 only");
 			printMenu();
 				}
 			return userSelection;
-                        
+
 }
 
-    
+
     public static double handleMenu(int userOption){
         Scanner sc=new Scanner(System.in);
         double kms=kiloMeters();
         double charges1=1.15*kms ;
         double charges2=1.08*kms ;
-        double charges3=1.35*kms ; 
+        double charges3=1.35*kms ;
     switch(userOption){
 	case 1: {
 		System.out.println("Your UBER fare is A$" + charges1);
                 System.out.println("Thank you for using the system and good bye>>>>>>");
                 break;
 	}
-	
+
 	case 2: {
 		System.out.println("Your DIDI fare is A$" + charges2);
                 System.out.println("Thank you for using the system and good bye>>>>>>");
                 break;
 	}
-	
+
 	case 3: {
 		System.out.println("Your OLA fare is A$" + charges3);
                 System.out.println("Thank you for using the system and good bye>>>>>>");
                 break;
 	}
-	
+
 	case 4: {
-		
+
 		System.out.println("Report");
                 System.out.println("\n\nName:" );
                 System.out.println("Total fare for UBER is A$" + charges1);
@@ -120,15 +120,11 @@ public static int printMenu() {
 	}
 	break;
 	case 5: {
-		
+
 		System.out.println("Thank you for using the system and good bye>>>>>>");
 	}
 	}return charges1;
 }
 
-    
-}	
-	
 
-
-
+}
